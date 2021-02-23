e89bad84a9cb49211ff4b1493567130cccb6b104
JacqueSatan <38167139+JacqueSatan@users.noreply.github.com>
2021-02-23 11:36:56 +0100

    documented user

--- search/all_7.js
+++ search/all_7.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['operator_20const_20uint64_5ft_31',['operator const uint64_t',['../class_ethyme_1_1_structures_1_1_snowflake.html#a5e30af8a51dced45ef93887270b1c2e8',1,'Ethyme::Structures::Snowflake']]],
-  ['string_32',['string',['../class_ethyme_1_1_structures_1_1_snowflake.html#a0f8fdfec993a9d2c32cdbe83ed64023e',1,'Ethyme::Structures::Snowflake']]]
+  ['string_32',['string',['../class_ethyme_1_1_structures_1_1_snowflake.html#a0f8fdfec993a9d2c32cdbe83ed64023e',1,'Ethyme::Structures::Snowflake::string()'],['../class_ethyme_1_1_structures_1_1_user.html#aecc0d3ab53abcebb702e02cbba1b609d',1,'Ethyme::Structures::User::string()']]]
 ];
