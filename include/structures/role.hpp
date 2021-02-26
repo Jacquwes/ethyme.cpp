973e6257866356b9fd83ef08f32774bcb51f88bc
JacqueSatan <38167139+JacqueSatan@users.noreply.github.com>
2021-02-26 18:46:57 +0100

    Add role structure

---
+++ include/structures/role.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "common.hpp"
+#include "structures/entity.hpp"
+
+namespace Ethyme
+{
+	class Client;
+}
+
+namespace Ethyme::Structures
+{
+	class Guild;
+
+	class Role : public Entity
+	{
+	public:
+		Role(nlohmann::json const& data, Ethyme::Client const& client);
+
+		uint32_t const& Color() const;
+		std::string const& Name() const;
+		uint32_t const& Permissions() const;
+		uint16_t const& Position() const;
+
+	private:
+		uint32_t m_color;
+		std::string m_name;
+		uint32_t m_permissions;
+		uint16_t m_position;
+	};
+}
---
+++ src/structures/role.cpp
@@ -0,0 +1,20 @@
+#include "structures/role.hpp"
+
+#include "client.hpp"
+#include "structures/guild.hpp"
+
+namespace Ethyme::Structures
+{
+	Role::Role(nlohmann::json const& data, Ethyme::Client const& client)
+		: Entity(data["id"].get<std::string>(), client)
+		, m_color{ data["color"].get<uint32_t>() }
+		, m_name{ data["name"].get<std::string>() }
+		, m_permissions{ data["permissions"].get<uint32_t>() }
+		, m_position{ data["position"].get<uint16_t>() }
+	{}
+
+	uint32_t const& Role::Color() const { return m_color; }
+	std::string const& Role::Name() const { return m_name; }
+	uint32_t const& Role::Permissions() const { return m_permissions; }
+	uint16_t const& Role::Position() const { return m_position; }
+}
