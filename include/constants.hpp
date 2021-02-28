eda0a42101f095fd51cad7f70acd9a9a53473cb7
JacqueSatan <38167139+JacqueSatan@users.noreply.github.com>
2021-02-28 12:16:04 +0100

    Add Intents to Constants

--- include/constants.hpp
+++ include/constants.hpp
@@ -16,6 +16,29 @@ namespace Ethyme::Constants
 		const std::string CurrentUser = Users + "/@me";
 	}
 
+	namespace Intents
+	{
+		enum
+		{
+			Guilds					= 1 << 0,
+			GuildMembers			= 1 << 1,
+			GuildBans				= 1 << 2,
+			GuildEmojis				= 1 << 3,
+			GuildIntegrations		= 1 << 4,
+			GuildWebhooks			= 1 << 5,
+			GuildInvites			= 1 << 6,
+			GuildVoiceStates		= 1 << 7,
+			GuildPresences			= 1 << 8,
+			GuildMessages			= 1 << 9,
+			GuildMessageReactions	= 1 << 10,
+			GuildMessageTyping		= 1 << 11,
+			DirectMessages			= 1 << 12,
+			DirectMessageReactions	= 1 << 13,
+			DirectMessageTyping		= 1 << 14
+	
+		};
+	}
+
 	namespace Permissions
 	{
 		enum
