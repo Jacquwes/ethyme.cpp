50376ac207d9952247278a09cdcbeef2da6b4470
JacqueSatan <38167139+JacqueSatan@users.noreply.github.com>
2021-04-14 15:01:59 +0200

    Add Parse, and handle data

--- include/structures/entity.hpp
+++ include/structures/entity.hpp
@@ -21,7 +21,7 @@ namespace Ethyme::Structures
 		 * @param id ID of the entity
 		 * @param client Client's instance
 		*/
-		Entity(std::string const& id, Ethyme::Client& client, bool const& partial = false);
+		Entity(std::string const& id, Ethyme::Client& client, bool const& partial = false, nlohmann::json const& data = {});
 		/**
 		 * @brief Inheritable
 		*/
@@ -31,25 +31,31 @@ namespace Ethyme::Structures
 		 * @brief Entity's ID
 		 * @return Entity's ID
 		*/
-		Snowflake const& Id() const;
+		constexpr inline Snowflake const& Id() const { return m_id; }
 		/**
 		 * @brief Client's instance
 		 * @return Client's instance
 		*/
-		Ethyme::Client& Client() const;
-		bool const& Partial() const;
+		constexpr inline Ethyme::Client& Client() const { return m_client; }
+
+		constexpr inline virtual void Parse(nlohmann::json const& data = {}) {}
+
+		constexpr inline bool const& Partial() const { return m_partial; }
 		/**
 		 * @brief Use this Entity as something else
 		 * @tparam T New type
 		 * @return This
 		*/
 		template<typename T>
-		inline T& As() const
+		constexpr inline T& As() const
 		{
 			return (T&)*this;
 		}
 
+		constexpr inline nlohmann::json& Data() { return m_data; }
+
 	private:
+		nlohmann::json m_data;
 		Snowflake m_id;
 		Ethyme::Client& m_client;
 		bool m_partial;
