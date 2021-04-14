0412860158ae0b46c21e8c0b2911dae63f1df129
JacqueSatan <38167139+JacqueSatan@users.noreply.github.com>
2021-04-14 14:58:50 +0200

    Add all channels types, create parse function, new collection

--- include/structures/guild.hpp
+++ include/structures/guild.hpp
@@ -20,16 +20,18 @@ namespace Ethyme
 		class Guild : public Entity
 		{
 		public:
-			Guild(nlohmann::json const& data, Ethyme::Client& client);
+			Guild(nlohmann::json const& data, Ethyme::Client& client, bool const& parse = true);
 
-			Collections::Collection<std::reference_wrapper<Channels::Channel>>& Channels();
+			Collections::Collection<Channels::Channel, true>& Channels();
 			Collections::Collection<Member>& Members();
 			std::string const& Name() const;
 			Member& Owner();
 			Collections::Collection<Role>& Roles();
 
+			void Parse(nlohmann::json const& data = {}) override;
+
 		private:
-			Collections::Collection<std::reference_wrapper<Channels::Channel>> m_channels;
+			Collections::Collection<Channels::Channel, true> m_channels;
 			Collections::Collection<Member> m_members;
 			std::string m_name;
 			std::string m_ownerId;
--- src/structures/guild.cpp
+++ src/structures/guild.cpp
@@ -2,41 +2,93 @@
 
 #include "client.hpp"
 #include "structures/channels/channel.hpp"
-#include "structures/channels/textchannel.hpp"
+#include "structures/channels/directmessage.hpp"
+#include "structures/channels/groupdirectmessage.hpp"
+#include "structures/channels/guildcategory.hpp"
+#include "structures/channels/guildnews.hpp"
+#include "structures/channels/guildstore.hpp"
+#include "structures/channels/guildtext.hpp"
+#include "structures/channels/guildvoice.hpp"
 #include "structures/member.hpp"
 #include "structures/role.hpp"
 
 namespace Ethyme::Structures
 {
-	Guild::Guild(nlohmann::json const& data, Ethyme::Client& client)
-		: Entity(data["id"].get<std::string>(), client)
+	Guild::Guild(nlohmann::json const& data, Ethyme::Client& client, bool const& parse)
+		: Entity(data["id"].get<std::string>(), client, !parse, data)
 		, m_channels{ client, Constants::API::Channels }
 		, m_members{ client, Constants::API::Guilds + Id().ToString() + "/members/" }
 		, m_name{ data["name"].get<std::string>() }
 		, m_roles{ client, Constants::API::Guilds + Id().ToString() + "/roles/" }
 		, m_ownerId{ data["owner_id"].get<std::string>() }
 	{
-		for (auto& role : data["roles"])
-			m_roles.Add(Role(role, client));
-		for (auto& channel : data["channels"])
-			switch (channel["type"].get<Channels::Channel::ChannelType>())
-			{
-			case Channels::Channel::ChannelType::DirectMessage:
-			case Channels::Channel::ChannelType::GroupDirectMessage:
-			case Channels::Channel::ChannelType::GuildText:
-			case Channels::Channel::ChannelType::GuildNews:
-				m_channels.Add(
-					Client().Channels().Add(Channels::TextChannel(channel, client))
-				);
-				break;
-			default:
-				break;
-			}
+		if (parse) [[unlikely]]
+			Parse(data);
 	}
 
-	Collections::Collection<std::reference_wrapper<Channels::Channel>>& Guild::Channels() { return m_channels; }
+	Collections::Collection<Channels::Channel, true>& Guild::Channels() { return m_channels; }
 	Collections::Collection<Member>& Guild::Members() { return m_members; }
 	std::string const& Guild::Name() const { return m_name; }
 	Member& Guild::Owner() { return *m_members.FindById(m_ownerId); }
 	Collections::Collection<Role>& Guild::Roles() { return m_roles; }
+
+	void Guild::Parse(nlohmann::json const& data)
+	{
+
+		if (!data.is_null()) [[unlikely]]
+			Data()[0] = data;
+
+		for (auto& role : Data()[0]["roles"])
+			m_roles.Add(Role(role, Client()));
+		for (auto& channel : Data()[0]["channels"])
+		{
+			if (!channel.contains("guild_id"))
+				const_cast<nlohmann::json&>(channel)["guild_id"] = Data()[0]["id"].get<std::string>();
+			switch (channel["type"].get<Channels::Channel::ChannelType>())
+			{
+			case Channels::Channel::ChannelType::DirectMessage:
+				m_channels.Add
+				(
+					Client().Channels().Add(Channels::DirectMessage(channel, Client()))
+				);
+				break;
+			case Channels::Channel::ChannelType::GroupDirectMessage:
+				m_channels.Add
+				(
+					Client().Channels().Add(Channels::GroupDirectMessage(channel, Client()))
+				);
+				break;
+			case Channels::Channel::ChannelType::GuildCategory:
+				m_channels.Add
+				(
+					Client().Channels().Add(Channels::GuildCategory(channel, Client()))
+				);
+				break;
+			case Channels::Channel::ChannelType::GuildNews:
+				m_channels.Add
+				(
+					Client().Channels().Add(Channels::GuildNews(channel, Client()).As<Channels::Channel>())
+				);
+				break;
+			case Channels::Channel::ChannelType::GuildStore:
+				m_channels.Add
+				(
+					Client().Channels().Add(Channels::GuildStore(channel, Client()))
+				);
+				break;
+			case Channels::Channel::ChannelType::GuildText:
+				m_channels.Add
+				(
+					Client().Channels().Add(Channels::GuildText(channel, Client()).As<Channels::Channel>())
+				);
+				break;
+			default:
+				m_channels.Add
+				(
+					Client().Channels().Add(Channels::Channel(channel, Client()))
+				);
+				break;
+			}
+		}
+	}
 }
