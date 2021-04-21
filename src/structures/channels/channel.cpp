981eb89de12c309193dc2070dd8357c311a917ab
JacqueSatan <38167139+JacqueSatan@users.noreply.github.com>
2021-04-21 21:06:26 +0200

    Handle null data in json

--- src/structures/channels/channel.cpp
+++ src/structures/channels/channel.cpp
@@ -3,8 +3,8 @@
 namespace Ethyme::Structures::Channels
 {
 	Channel::Channel(nlohmann::json const& data, Ethyme::Client& client)
-		: m_channelType(data["type"].get<ChannelType>())
-		, Entity(data["id"].get<std::string>(), client, false, data)
+		: m_channelType(data.contains("type") ? data["type"].get<ChannelType>() : ChannelType::GuildText)
+		, Entity(data.contains("id") ? data["id"].get<std::string>() : "000000000000000000", client, false, data)
 	{}
 
 	const Channel::ChannelType& Channel::Type() const
--- src/structures/channels/guildtext.cpp
+++ src/structures/channels/guildtext.cpp
@@ -4,23 +4,23 @@
 
 namespace Ethyme::Structures::Channels
 {
-    GuildText::GuildText(nlohmann::json const& data, Ethyme::Client& client)
-        : GuildChannel{ data, client }
-        , TextChannel{ data, client }
-        , m_parent{
-            this->Guild().Channels().Find(
-                [&](Channel& c)
-                {
-                    return data["parent_id"] == c.Id();
-                })->As<GuildCategory>()
-        }
-        , m_nsfw{ data["nsfw"] }
-        , m_topic{ data.contains("topic") ? data["topic"] : "" }
-    {}
+	GuildText::GuildText(nlohmann::json const& data, Ethyme::Client& client)
+		: GuildChannel{ data, client }
+		, TextChannel{ data, client }
+		, m_parent{
+			data.contains("parent_id") && !data["parent_id"].is_null()
+			? this->Guild().Channels().Find(
+				[&](Channel& c)
+				{
+					return data["parent_id"] == c.Id();
+				})->As<GuildCategory>()
+			: const_cast<Channels::Channel&>(this->As<Channel>().Client().UnknownChannel()).As<GuildCategory>()
+		}
+		, m_nsfw{ data.contains("nsfw") ? data["nsfw"].get<bool>() : false }
+		, m_topic{ data.contains("topic") && !data["topic"].is_null() ? data["topic"] : "" }
+	{}
 
-                std::array<std::string, 5> slt();
-
-    GuildCategory& GuildText::Parent() { return m_parent; }
-    bool const& GuildText::Nsfw() const { return m_nsfw; }
-    std::string const& GuildText::Topic() const { return m_topic; }
+	GuildCategory& GuildText::Parent() { return m_parent; }
+	bool const& GuildText::Nsfw() const { return m_nsfw; }
+	std::string const& GuildText::Topic() const { return m_topic; }
 }
